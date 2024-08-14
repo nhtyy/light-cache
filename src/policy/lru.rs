d6fc84efde9fc4cfbd3811b32b78b56dcba0cc19
frizellle <jacobfrizelle@g.ucla.edu>
2024-08-13 21:23:11 -0700

    TLRU implementation and tests

--- src/policy/lru.rs
+++ src/policy/lru.rs
@@ -3,7 +3,11 @@ use std::{
     hash::{BuildHasher, Hash},
     marker::PhantomData,
     sync::{Arc, Mutex, MutexGuard},
+    time::{Duration, Instant},
+    cmp::Reverse,
 };
+
+use priority_queue::PriorityQueue;
 
 use super::{
     linked_arena::{LinkedArena, LinkedNode},
@@ -28,21 +32,36 @@ impl<K, V> Clone for LruPolicy<K, V> {
 }
 
 pub struct LruPolicyInner<K> {
-    size: u64,
-    capacity: u64,
+    capacity: usize,
+    node_lifetime: Option<Duration>,
     arena: LinkedArena<K, LruNode<K>>,
+    expiring: Option<PriorityQueue<K, Reverse<Instant>>>,
 }
 
-impl<K, V> LruPolicy<K, V> {
-    pub fn new(capacity: u64) -> Self {
-        LruPolicy {
-            inner: Arc::new(Mutex::new(LruPolicyInner {
-                size: 0,
-                capacity,
-                arena: LinkedArena::new(),
-            })),
-            phantom: PhantomData,
+impl<K: Hash + Eq, V> LruPolicy<K, V> {
+    pub fn new(capacity: usize, node_lifetime: Option<Duration>) -> Self {
+        if let Some(_) = node_lifetime {
+            LruPolicy {
+                inner: Arc::new(Mutex::new(LruPolicyInner {
+                    capacity,
+                    node_lifetime,
+                    arena: LinkedArena::new(),
+                    expiring: Some(PriorityQueue::new()),
+                })),
+                phantom: PhantomData,
+            }
+        } else {
+            LruPolicy {
+                inner: Arc::new(Mutex::new(LruPolicyInner {
+                    capacity,
+                    node_lifetime: None,
+                    arena: LinkedArena::new(),
+                    expiring: None,
+                })),
+                phantom: PhantomData,
+            }
         }
+        
     }
 }
 
@@ -59,10 +78,14 @@ where
     }
 
     fn get<S: BuildHasher>(&self, key: &K, cache: &LightCache<K, V, S, Self>) -> Option<V> {
-        let mut inner = self.lock_inner();
+        {
+            let mut inner = self.lock_inner();
+            inner.prune(cache);
 
-        if let Some((idx, _)) = inner.arena.get_node_mut(&key) {
-            inner.arena.move_to_head(idx);
+            if let Some((idx, node)) = inner.arena.get_node_mut(&key) {
+                let mut inner = self.lock_inner();
+                inner.arena.move_to_head(idx);
+            }
         }
 
         cache.get_no_policy(key)
@@ -71,13 +94,17 @@ where
     fn insert<S: BuildHasher>(&self, key: K, value: V, cache: &LightCache<K, V, S, Self>) -> Option<V> {
         {
             let mut inner = self.lock_inner();
+            inner.prune(cache);
 
             // were updating the value, so lets reset the creation time
-            if let Some((idx, _)) = inner.arena.get_node_mut(&key) {
+            if let Some((idx, node)) = inner.arena.get_node_mut(&key) {
                 inner.arena.move_to_head(idx);
             } else {
                 inner.arena.insert_head(key);
-                inner.size += 1;
+            }
+
+            if let Some(pq) = inner.expiring.as_mut() {
+                pq.push(key, Reverse(Instant::now()));
             }
 
             inner.prune(cache);
@@ -90,7 +117,10 @@ where
         {
             let mut inner = self.lock_inner();
             inner.arena.remove_item(key);
-            inner.size -= 1;
+
+            if let Some(pq) = inner.expiring.as_mut() {
+                pq.remove(key);
+            }
         }
 
         cache.remove_no_policy(key)
@@ -104,12 +134,26 @@ where
 {
     fn prune<S: BuildHasher>(&mut self, cache: &LightCache<K, V, S, LruPolicy<K, V>>) {
         while let Some((idx, _)) = self.arena.tail() {
-            if self.size > self.capacity {
+            if self.arena.len() > self.capacity {
                 let (_, n) = self.arena.remove(idx);
-                self.size -= 1;
                 cache.remove_no_policy(n.item());
+                if let Some(pq) = self.expiring.as_mut() {
+                    pq.remove(&n.key);
+                }
             } else {
                 break;
+            }
+        }
+        if let Some(pq) = self.expiring.as_mut() {
+            while let Some(top) = pq.peek() {
+                let now = Instant::now();
+                if now.duration_since(top.1.0) > self.node_lifetime.expect("lifetime exists if queue does") {
+                    self.arena.remove_item(top.0);
+                    cache.remove_no_policy(top.0);
+                    pq.pop();
+                } else {
+                    break;
+                }
             }
         }
     }
@@ -117,6 +161,7 @@ where
 
 pub struct LruNode<K> {
     key: K,
+    creation: Instant,
     prev: Option<usize>,
     next: Option<usize>,
 }
@@ -128,6 +173,7 @@ where
     fn new(key: K, prev: Option<usize>, next: Option<usize>) -> Self {
         LruNode {
             key,
+            creation: Instant::now(),
             prev,
             next,
         }
@@ -169,6 +215,14 @@ mod test {
 
     use super::*;
 
+    fn duration_seconds(seconds: u64) -> Duration {
+        Duration::from_secs(seconds)
+    }
+
+    fn sleep_seconds(seconds: u64) {
+        std::thread::sleep(duration_seconds(seconds));
+    }
+
     fn insert_n<S>(cache: &LightCache<i32, i32, S, LruPolicy<i32, i32>>, n: usize)
     where
         S: BuildHasher,
@@ -178,35 +232,37 @@ mod test {
         }
     }
 
-    fn cache<K, V>(capacity: u64) -> LightCache<K, V, DefaultHashBuilder, LruPolicy<K, V>>
+    fn cache<K, V>(capacity: usize, lifetime: Duration) -> LightCache<K, V, DefaultHashBuilder, LruPolicy<K, V>>
     where
         K: Copy + Eq + Hash,
         V: Clone + Sync,
     {
-        LightCache::from_parts(LruPolicy::new(capacity), Default::default())
+        LightCache::from_parts(LruPolicy::new(capacity, Some(lifetime)), Default::default())
     }
 
     #[test]
     /// Insert 5 keys, and insert 2 more keys
     /// this will leave only the last 2 items inserted
     fn test_basic_scenario_1() {
-        let cache = cache::<i32, i32>(2);
+        let cache = cache::<i32, i32>(5, duration_seconds(1));
 
         insert_n(&cache, 5);
+
+        sleep_seconds(2);
+
         insert_n(&cache, 2);
        
-        // 1 should be removed by now
         assert_eq!(cache.len(), 2);
         let policy = cache.policy().lock_inner();
 
         assert_eq!(policy.arena.nodes.len(), 2);
-        assert_eq!(policy.arena.head, Some(0));
-        assert_eq!(policy.arena.tail, Some(1));
+        assert_eq!(policy.arena.head, Some(1));
+        assert_eq!(policy.arena.tail, Some(0));
     }
 
     #[test]
     fn test_basic_scenario_2() {
-        let cache = cache::<i32, i32>(5);
+        let cache = cache::<i32, i32>(5, duration_seconds(2));
 
         insert_n(&cache, 10);
         cache.remove(&8);
